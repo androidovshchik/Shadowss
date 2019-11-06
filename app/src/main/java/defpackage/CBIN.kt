aebdfe430da073cf201b1b17e1d34e7fc070f212
vlad <vladkalyuzhnyu@gmail.com>
2019-11-06 19:34:46 +0300

    init

--- app/src/main/java/defpackage/CBIN.kt
+++ app/src/main/java/defpackage/CBIN.kt
@@ -1,5 +1,7 @@
 package defpackage
 
+import timber.log.Timber
+
 /**
  * Важны имена всего, тоже и в смежных классах
  */
@@ -11,14 +13,29 @@ object CBIN {
     }
 
     fun marshal(obj: Any): ByteArray? {
-        return main_marshal(obj, obj.javaClass.name)
+        val bytes = ByteArray()
+        try {
+            val result = main_marshal(obj, bytes)
+            Timber.d("marshal $result")
+        } catch (e: Throwable) {
+            Timber.e(e)
+        }
+        return bytes
     }
 
-    inline fun <reified T> unmarshal(bytes: ByteArray): Any? {
-        return main_unmarshal(bytes, T::class.java.name)
+    inline fun <reified T : Any> unmarshal(bytes: ByteArray): Any {
+        val obj = T::class.java.getConstructor(T::class.java)
+            .newInstance()
+        try {
+            val result = main_unmarshal(obj, bytes)
+            Timber.d("unmarshal $result")
+        } catch (e: Throwable) {
+            Timber.e(e)
+        }
+        return obj
     }
 
-    private external fun main_marshal(obj: Any, className: String): ByteArray?
+    private external fun main_marshal(obj: Any, bytes: ByteArray): Int
 
-    external fun main_unmarshal(bytes: ByteArray, className: String): Any?
+    external fun main_unmarshal(obj: Any, bytes: ByteArray): Int
 }
