1e07be36640363851d61219228e2eeed22a51488
vlad <vladkalyuzhnyu@gmail.com>
2019-11-08 17:01:01 +0300

    changes

--- app/src/main/java/domain/shadowss/MainApp.kt
+++ app/src/main/java/domain/shadowss/MainApp.kt
@@ -1,17 +1,24 @@
 package domain.shadowss
 
 import android.app.Application
+import android.content.Context
 import domain.shadowss.data.local.localModule
 import domain.shadowss.data.remote.remoteModule
 import domain.shadowss.extension.noopInit
 import domain.shadowss.manager.managerModule
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.provider
 
 @Suppress("unused")
 class MainApp : Application(), KodeinAware {
 
     override val kodein by Kodein.lazy {
+
+        bind<Context>() with provider {
+            applicationContext
+        }
 
         import(localModule)
 
